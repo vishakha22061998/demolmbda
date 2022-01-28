f3e29fdcd2a7ec277928b8afa48a1eca6f9706da
vishakha22061998 <98169297+vishakha22061998@users.noreply.github.com>
2022-01-28 13:36:51 +0530

    Update and rename src/main/java/com/lambda1/SesSample.java to src/main/java/com/Sample.java

---
+++ src/main/java/com/Sample.java
@@ -0,0 +1,16 @@
+package com.lambda1;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Sample implements RequestHandler<Object,String>
+{
+	@Override
+	public String handleRequest(Object input, Context context) {
+		context.getLogger().log("Input:" + input);
+		//System.out.println("Hello World!");
+		return "Hello World!............";
+	}
+
+	}
+	
